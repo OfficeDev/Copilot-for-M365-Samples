b16089d7aec6348393160c0f406d135b7888bf00
Garry Trinder <garry@trinder365.co.uk>
2023-11-15 15:19:46 +0000

    Add msgext-mulitparam-ts sample

---
+++ samples/msgext-multiparam-ts/src/types.ts
@@ -0,0 +1,12 @@
+export type StockData = {
+    companyName: string;
+    primaryExchange: string;
+    symbol: string;
+    latestUpdate: number;
+    change: number;
+    changePercent: number;
+    latestPrice: number;
+    open: number;
+    high: number;
+    low: number;
+}
