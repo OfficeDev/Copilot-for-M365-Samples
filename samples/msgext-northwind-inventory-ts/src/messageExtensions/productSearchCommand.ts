d212dd1dd1193b18fce1719db5a53911d3429728
Bob German <rogerman@microsoft.com>
2023-12-08 09:13:46 -0800

    Another update for named params

--- samples/msgext-northwind-inventory-ts/src/messageExtensions/productSearchCommand.ts
+++ samples/msgext-northwind-inventory-ts/src/messageExtensions/productSearchCommand.ts
@@ -15,17 +15,21 @@ async function handleTeamsMessagingExtensionQuery(
     query: MessagingExtensionQuery
 ): Promise<MessagingExtensionResponse> {
 
+    let productName, categoryName, inventoryStatus, supplierCity, stockLevel;
+
     // For now we have the ability to pass parameters comma separated for testing until the UI supports it.
     // So try to unpack the parameters but when issued from Copilot or the multi-param UI they will come
     // in the parameters array.
-    let [productName, categoryName, inventoryStatus, supplierCity, stockLevel] = (query.parameters[0]?.value.split(','));
-
-    productName ??= cleanupParam(query.parameters.find((element) => element.name === "productName")?.value);
-    categoryName ??= cleanupParam(query.parameters.find((element) => element.name === "categoryName")?.value);
-    inventoryStatus ??= cleanupParam(query.parameters.find((element) => element.name === "inventoryStatus")?.value);
-    supplierCity ??= cleanupParam(query.parameters.find((element) => element.name === "supplierCity")?.value);
-    stockLevel ??= cleanupParam(query.parameters.find((element) => element.name === "stockQuery")?.value);
-    console.log(`🔎 Query #${++queryCount}:\nproductName=${productName}, categoryName=${categoryName}, inventoryStatus=${inventoryStatus}, supplierCity=${supplierCity}, stockLevel=${stockLevel}`);
+    if (query.parameters.length === 1 && query.parameters[0]?.name === "productName") {
+        [productName, categoryName, inventoryStatus, supplierCity, stockLevel] = (query.parameters[0]?.value.split(','));
+    } else { 
+        productName = cleanupParam(query.parameters.find((element) => element.name === "productName")?.value);
+        categoryName = cleanupParam(query.parameters.find((element) => element.name === "categoryName")?.value);
+        inventoryStatus = cleanupParam(query.parameters.find((element) => element.name === "inventoryStatus")?.value);
+        supplierCity = cleanupParam(query.parameters.find((element) => element.name === "supplierCity")?.value);
+        stockLevel = cleanupParam(query.parameters.find((element) => element.name === "stockQuery")?.value);
+    }
+    console.log(`🔎 Query #${++queryCount}:\nproductName=${productName}, categoryName=${categoryName}, inventoryStatus=${inventoryStatus}, supplierCity=${supplierCity}, stockLevel=${stockLevel}`);    
 
     const products = await searchProducts(productName, categoryName, inventoryStatus, supplierCity, stockLevel);
 
