84755e53fb6e5363af288c2bba39a7089e8e6e40
Garry Trinder <garry@trinder365.co.uk>
2024-01-30 10:18:03 +0000

    Fix issue editing Title field. Closes #73 (#74)

--- samples/msgext-product-support-sso-csharp/Models/ActionData.cs
+++ samples/msgext-product-support-sso-csharp/Models/ActionData.cs
@@ -3,9 +3,8 @@
     public class ActionData
     {
         public string Id { get; set; }
-
+        public string Title { get; set; }
         public string RetailCategory { get; set; }
-
         public DateTime ReleaseDate { get; set; }
     }
 }
