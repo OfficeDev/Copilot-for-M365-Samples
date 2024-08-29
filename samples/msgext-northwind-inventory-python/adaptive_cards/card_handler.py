255b9545b6b91b4d58fea46f143679f32f02a367
Jegadeesh V <v-jegadeeshv@microsoft.com>
2024-08-29 15:08:13 +0530

    Fixing PR comments related to Azurite

--- samples/msgext-northwind-inventory-python/adaptive_cards/card_handler.py
+++ samples/msgext-northwind-inventory-python/adaptive_cards/card_handler.py
@@ -12,7 +12,7 @@ async def handle_teams_card_action_update_stock(context: TurnContext):
         product = await get_product_ex(data["productId"])
         product["UnitsInStock"] = int(data["txtStock"])
         await update_product(product)
-        message = f"Stock updated for {product["ProductName"]} to {product["UnitsInStock"]}!"
+        message = f"Stock updated for {product['ProductName']} to {product['UnitsInStock']}!"
         edit_card = cards.success_card(product, message)
         return create_adaptive_card_invoke_response(200, edit_card)
     else:
@@ -27,7 +27,7 @@ async def handle_teams_card_action_cancel_restock(context: TurnContext):
         product = await get_product_ex(data["productId"])
         product["UnitsOnOrder"] = 0
         await update_product(product)
-        message = f"Restock cancelled for {product["ProductName"]}."
+        message = f"Restock cancelled for {product['ProductName']}."
         edit_card = cards.success_card(product, message)
         return create_adaptive_card_invoke_response(200, edit_card)
     else:
@@ -42,7 +42,7 @@ async def handle_teams_card_action_restock(context: TurnContext):
         product = await get_product_ex(data["productId"])
         product["UnitsOnOrder"] = int(product["UnitsOnOrder"]) + int(data["txtStock"])
         await update_product(product)
-        message = f"Restocking {product["ProductName"]} placed order for {product["UnitsOnOrder"]} units."
+        message = f"Restocking {product['ProductName']} placed order for {product['UnitsOnOrder']} units."
         edit_card = cards.success_card(product, message)
         return create_adaptive_card_invoke_response(200, edit_card)
     else:
